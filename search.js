58926dc36ad24e30dd20ea15d6ff6b55a3e51bc9
Will Davis <twd38@cornell.edu>
2018-05-11 17:26:34 -0400

    looking dope

--- search.js
+++ search.js
@@ -23,16 +23,39 @@ function getObjects(obj, key, val) {
     return objects;
 }
 
+const debounce = (duration, fn) => {
+  const state = { lastCall : Date.now() }
+  let debouncedInterval = null
+
+  return (...args) => {
+    clearInterval(debouncedInterval);
+    console.log('debounced')
+    debouncedInterval = setTimeout(() => {
+      console.log('called')
+      fn(...args);
+    }, duration)
+
+  }
+}
+
 document.addEventListener('DOMContentLoaded', () => {
   document.getElementById('topicSearch').addEventListener('keypress', function(evt) {
     // split value to keywords, search for every keyword
     const keyword = this.value.toLowerCase();
-    const filtered_tweets = tweets.filter(tweet => {
-      return tweet.Tweets.toLowerCase().indexOf(keyword)!= -1;
-    })
-    updateResults(filtered_tweets);
+    if (keyword.length < 3) {
+      return $('#results').html(`<h4 class="text-center text-danger">Please narrow your search</h4>`)
+    }
+    $('#results').html(`<div class="loader mx-auto"></div>`)
+    applyFilterOnTweets(keyword);
   });
 });
+
+const applyFilterOnTweets = debounce(500, (keyword) => {
+  const filtered_tweets = tweets.filter(tweet => {
+    return tweet.Tweets.toLowerCase().indexOf(keyword)!= -1;
+  })
+  updateResults(filtered_tweets);
+})
 
 const updateResults = (filteredTweets) => {
   // create element for every tweet
@@ -40,16 +63,23 @@ const updateResults = (filteredTweets) => {
   filteredTweets.forEach(tweet => {
     // $('#results').append(`<div>${tweet.Tweets}</div>`)
     $('#results').append(`<ul class="list-group">
-      <li class="list-group-item">${tweet["Official Name"] + " : " + tweet.Tweets}</li>
+      <li class="list-group-item mx-auto" style="width: 1000px;">
+
+
+          <p class=" d-inline font-weight-bold">${tweet["Official Name"]}</p>
+          <p class="d-inline font-weight-light text-primary"> @ ${tweet["Twitter Handle"]}</p>
+
+        <p>${tweet.Tweets}</p>
+      </li>
     </ul>`)
 
   })
-  console.log(filteredTweets.length)
+  // console.log(filteredTweets.length)
 }
 
 
 
-$.getJSON("https://api.myjson.com/bins/17gwhy", function(json) {
+$.getJSON("https://raw.githubusercontent.com/twd38/twd38.github.io/master/tweets.json", function(json) {
 
  tweets = json;
  // console.log(tweets);
