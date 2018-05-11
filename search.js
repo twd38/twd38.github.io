d848f5276f8f22f0f8558abbd5e92d640ba3bd44
Will Davis <twd38@cornell.edu>
2018-05-11 19:59:39 -0400

    added filters

--- search.js
+++ search.js
@@ -2,26 +2,9 @@
 
 // var search = new JsSearch.Search('Official Name');
 var tweets = []
-
-function getObjects(obj, key, val) {
-    var objects = [];
-    for (var i in obj) {
-        if (!obj.hasOwnProperty(i)) continue;
-        if (typeof obj[i] == 'object') {
-            objects = objects.concat(getObjects(obj[i], key, val));
-        } else
-        //if key matches and value matches or if key matches and value is not passed (eliminating the case where key matches but passed value does not)
-        if (i == key && obj[i] == val || i == key && val == '') { //
-            objects.push(obj);
-        } else if (obj[i] == val && key == ''){
-            //only add if the object is not already in the array
-            if (objects.lastIndexOf(obj) == -1){
-                objects.push(obj);
-            }
-        }
-    }
-    return objects;
-}
+var filteredTweetsSave = []
+var filteredTweetsName = []
+var filterMethod = ""
 
 const debounce = (duration, fn) => {
   const state = { lastCall : Date.now() }
@@ -34,18 +17,47 @@ const debounce = (duration, fn) => {
       console.log('called')
       fn(...args);
     }, duration)
-
   }
 }
+
+jQuery("#none").click(function(e){
+  filterMethod = "none";
+  console.log("Filter name");
+  const keyword = document.getElementById('topicSearch').value.toLowerCase();
+  applyFilterOnTweets(keyword);
+
+e.preventDefault();
+});
+
+jQuery("#Name").click(function(e){
+  filterMethod = "Name";
+  console.log("Filter name");
+  const keyword = document.getElementById('topicSearch').value.toLowerCase();
+  applyFilterOnTweets(keyword);
+
+e.preventDefault();
+});
+
+jQuery("#Date").click(function(e){
+  filterMethod = "Date";
+  console.log("Filter name");
+  const keyword = document.getElementById('topicSearch').value.toLowerCase();
+  applyFilterOnTweets(keyword);
+
+e.preventDefault();
+});
+
+
+
 
 document.addEventListener('DOMContentLoaded', () => {
   document.getElementById('topicSearch').addEventListener('keypress', function(evt) {
     // split value to keywords, search for every keyword
     const keyword = this.value.toLowerCase();
     if (keyword.length < 2) {
-      return $('#results').html(`<h4 class="text-center text-danger">Please narrow your search</h4>`)
+      return $('#results_ul').html(`<h4 class="text-center text-danger">Please narrow your search</h4>`)
     }
-    $('#results').html(`<div class="loader mx-auto"></div>`)
+    $('#results_ul').html(`<div class="loader mx-auto"></div>`)
     applyFilterOnTweets(keyword);
   });
 });
@@ -59,22 +71,123 @@ const applyFilterOnTweets = debounce(500, (keyword) => {
 
 const updateResults = (filteredTweets) => {
   // create element for every tweet
-  $('#results').html("")
+  $('#results_ul').html("")
+  filteredTweetsSave = filteredTweets
   filteredTweets.forEach(tweet => {
     // $('#results').append(`<div>${tweet.Tweets}</div>`)
-    $('#results').append(`<ul class="list-group">
-      <li class="list-group-item mx-auto" style="width: 1000px;">
+
+    if(filterMethod == ""){
+      $('#results_ul').append(`
+        <li class="list-group-item mx-auto" style="width: 1000px;">
 
 
-          <p class=" d-inline font-weight-bold">${tweet["Official Name"]}</p>
-          <p class="d-inline font-weight-light text-primary"> @ ${tweet["Twitter Handle"]}</p>
+            <p class=" d-inline font-weight-bold">${tweet["Official Name"]}</p>
+            <p class="d-inline font-weight-light text-primary"> @${tweet["Twitter Handle"]}</p>
 
-        <p>${tweet.Tweets}</p>
-      </li>
-    </ul>`)
+          <p>${tweet.Tweets}</p>
+        </li>
+      `)
+    }
 
-  })
-  // console.log(filteredTweets.length)
+    if(filterMethod == "none"){
+      $('#results_ul').append(`
+        <li class="list-group-item mx-auto" style="width: 1000px;">
+
+
+            <p class=" d-inline font-weight-bold">${tweet["Official Name"]}</p>
+            <p class="d-inline font-weight-light text-primary"> @${tweet["Twitter Handle"]}</p>
+
+          <p>${tweet.Tweets}</p>
+        </li>
+      `)
+    }
+
+
+    if(filterMethod == "Name"){
+      // filteredTweetsSave.sort((tweet_a, tweet_b) => tweet_a["Official Name"] < tweet_b["Official Name"])
+
+      filteredTweetsSave.sort(function(tweet_a, tweet_b){
+      if(tweet_a["Official Name"].toLowerCase() < tweet_b["Official Name"].toLowerCase()) return -1;
+      if(tweet_a["Official Name"].toLowerCase() > tweet_b["Official Name"].toLowerCase()) return 1;
+      return 0;
+      })
+
+      $('#results_ul').append(`
+        <li class="list-group-item mx-auto" style="width: 1000px;">
+
+
+            <p class=" d-inline font-weight-bold">${tweet["Official Name"]}</p>
+            <p class="d-inline font-weight-light text-primary"> @${tweet["Twitter Handle"]}</p>
+
+          <p>${tweet.Tweets}</p>
+        </li>
+      `)
+    }
+
+    // //DATE FILTER
+    // if(filterMethod == "Date"){
+    //   // filteredTweetsSave.sort((tweet_a, tweet_b) => tweet_a["Official Name"] < tweet_b["Official Name"])
+    //
+    //   filteredTweetsSave.sort(function(tweet_a, tweet_b){
+    //   if(tweet_a["Date"] < tweet_b["Date"] return -1;
+    //   if(tweet_a["Date"] > tweet_b["Date"] return 1;
+    //   return 0;
+    //   })
+    //
+    //   $('#results_ul').append(`
+    //     <li class="list-group-item mx-auto" style="width: 1000px;">
+    //
+    //
+    //         <p class=" d-inline font-weight-bold">${tweet["Official Name"]}</p>
+    //         <p class="d-inline font-weight-light text-primary"> @${tweet["Twitter Handle"]}</p>
+    //
+    //       <p>${tweet.Tweets}</p>
+    //     </li>
+    //   `)
+    // }
+    //
+    // //AGENCY FILTER
+    // if(filterMethod == "Agency"){
+    //   filteredTweetsSave.sort(function(tweet_a, tweet_b){
+    //   if(tweet_a["Agency"].toLowerCase() < tweet_b["Agency"].toLowerCase()) return -1;
+    //   if(tweet_a["Agency"].toLowerCase() > tweet_b["Agency"].toLowerCase()) return 1;
+    //   return 0;
+    //   })
+    //
+    //   $('#results_ul').append(`
+    //     <li class="list-group-item mx-auto" style="width: 1000px;">
+    //
+    //
+    //         <p class=" d-inline font-weight-bold">${tweet["Official Name"]}</p>
+    //         <p class="d-inline font-weight-light text-primary"> @${tweet["Twitter Handle"]}</p>
+    //
+    //       <p>${tweet.Tweets}</p>
+    //     </li>
+    //   `)
+    // }
+
+
+
+
+
+
+    })
+    console.log(filteredTweetsSave)
+
+
+}
+
+
+function dynamicSort(property) {
+    var sortOrder = 1;
+    if(property[0] === "-") {
+        sortOrder = -1;
+        property = property.substr(1);
+    }
+    return function (a,b) {
+        var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
+        return result * sortOrder;
+    }
 }
 
 
@@ -82,17 +195,4 @@ const updateResults = (filteredTweets) => {
 $.getJSON("https://raw.githubusercontent.com/twd38/twd38.github.io/master/tweets.json", function(json) {
 
  tweets = json;
- // console.log(tweets);
-   //  console.log(json); // this will show the info it in firebug console
-   //
-
-
-
-
-
-
-     // console.log(getObjects(tweets,'tweets','Edmund Moy'));
 });
-
-// var json = require('/tweets.json');
-//var js = JSON.parse(json);
